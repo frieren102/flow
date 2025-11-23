915ab43beb357056c7b963f1cb481251243bd3f6
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-11-23 15:49:10 +0530

    changes

--- back/back1/back1/urls.py
+++ back/back1/back1/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "API is running"})
+
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('api/', include('api.urls')),
 ]
